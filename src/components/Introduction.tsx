1f0f631e1f6499c8078e0f0590ad1c4a454cd977
arthurnnguyen <arthurnnguyen@gmail.com>
2025-06-30 12:23:52 -0700

    demo ci/cd

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,7 +6,7 @@ function Introduction () {
         <div className='w-[5px] h-[5px] bg-green-600 rounded-full'></div>
         <h2 className='mt-5 text-[#AFAFB0] font-medium-text-[13px]
         tracking-widest'> Hello! My name is</h2>
-        <h2 className='text-[70px] font-bold text-[#AFAFB0] tracking-widest mt-5'>Arthur Nguyen</h2>
+        <h2 className='text-[70px] font-bold text-[#AFAFB0] tracking-widest mt-5'>This is a Demo!!!!!</h2>
         <h2 className='text-[20px] font-medium text-[#AFAFB0] tracking-widest'>Computer Science Senior</h2>
     </div>
   )
